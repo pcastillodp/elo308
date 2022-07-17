b72d14bf62ea67affe238fc17f4ace4b275eed9a
pcastillodp <polet.castillo.14@sansano.usm.cl>
2022-07-17 17:12:14 -0400

    tratando de funcionar

--- codigos_prueba/comunicacion_udp.py
+++ codigos_prueba/comunicacion_udp.py
@@ -13,20 +13,48 @@ import threading
     seguidor2 -> seguidor1 -> lider -> monitor
 """
 
-flag_robot = "L" #M:monitor ; L:lider ; S1:seguidor 1 ; S2:seguidor 2
+flag_robot = "S1" #M:monitor ; L:lider ; S1:seguidor 1 ; S2:seguidor 2
+
+#UDP
+bufferSize = 1024
+socket_udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+socket_udp.connect(("8.8.8.8", 80))
+data = bytearray()
 
 monitor = ("192.168.100.9", 1234)   #ip y puerto pc windows
 lider = ("192.168.100.20", 1111)    #ip y puerto de la raspberry
 seguidor1 = ("192.168.100.22", 1111)   #ip y puerto del robot sucesor 
 seguidor2 = ("192.168.100.23", 1111)   #ip y puerto del robot sucesor 
-t_actual = 0
-t_com_predecesor = 0
-t_reporte = 0
 
-#UDP
-bufferSize = 1024
-socket_udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-data = bytearray()
+def main(args=None):
+    global socket_udp, lider
+    
+    ip_local= socket_udp.getsockname()[0]
+    if (ip_local == "192.168.100.9"): flag_robot = "M"
+    elif(ip_local == "192.168.100.20"): flag_robot = "L"
+    elif(ip_local == "192.168.100.22"): flag_robot = "S1"
+    elif(ip_local == "192.168.100.23"): flag_robot = "S2"
+
+    print(socket_udp.getsockname()[0])
+
+    if(flag_robot=="M"):
+        print("soy el monitor")
+        print("escribe L/ para solicitarle datos al lider")
+        mensaje = input("escribe el mensaje a enviar: ")
+        if (mensaje == "L/"):
+            solicitar(lider)
+            t_udp = threading.Thread(target =  self.recibir, args =(lider) ) 
+            t_udp.setDaemon(True)
+            t_udp.start()
+
+    elif(flag_robot == "L"):
+        print("soy el lider")
+        print("estare atento al monitor por si solicitan datos")
+        t_udp = threading.Thread(target=recibir,args =(lider)) 
+        t_udp.setDaemon(True)
+        t_udp.start()
+
+
 
 def enviar (sujeto):
     print("enviar")
@@ -63,29 +91,6 @@ def recibir(sujeto):
         print('received {} bytes from {}'.format(len(data), address))
         print(data)
 
-
-def main(args=None):
-    global lider, seguidor1, seguidor2, flag_robot
-    if(flag_robot=="M"):
-        print("soy el monitor")
-        print("escribe L/ para solicitarle datos al lider")
-        mensaje = input("escribe el mensaje a enviar: ")
-        if (mensaje == "L/"):
-            solicitar(lider)
-            t_udp = threading.Thread(target =  recibir, args =(lider) ) 
-            t_udp.setDaemon(True)
-            t_udp.start()
-
-    elif(flag_robot == "L"):
-        print("soy el lider")
-        print("estare atento al monitor por si solicita datos")
-        t_udp = threading.Thread(target =  recibir, args =(lider) ) 
-        t_udp.setDaemon(True)
-        t_udp.start()
-        
-
-    elif (flag_robot == "S1"):
-        print("seguidor 1")
-    elif (flag_robot == "S2"):
-        print("seguidor 2")
-
+if __name__ == "__main__": 
+  main()
+  
