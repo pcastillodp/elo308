851beda5f9bbe16e8915dd88ea437440414b42e1
pcastillodp <polet.castillo.14@sansano.usm.cl>
2022-07-25 22:23:11 -0400

    se agrega utilidades

---
+++ utilities/udp_python_curva.py
@@ -0,0 +1,37 @@
+"""
+_____________________________________________________
+|   -Conexion de UDP con la base de datos en MySQL  |
+|    -Crear archivo CSV                             |
+|---------------------------------------------------|
+
+"""
+#Librerias
+import sys
+import pymysql
+import csv
+import socket
+#Ajustables
+file_name = "C:\\Users\\Poletarro\\Desktop\\Memoria\\servidor_udp\\monitoreo.csv"  # archivo csv
+
+UDP_IP = "192.168.100.9" # ip del computador que recibe datos (mismo que el que corre este script)
+UDP_PORT = 1234
+#UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+#creacion de archivo CSV
+texto = open(file_name,'w')
+#estado = "T,"+String(Input_d)+","+String(d_ref)+","+String(vel_ref)+","+String(Input_vel)+","+String(Input_theta)+","+String(Output_d)+","+String(Output_vel)+","+String(Output_theta);
+ 
+texto.write('Robot,Delta_muestra,Input_d,d_ref,vel_ref,Input_vel,Input_theta,Output_d,Output_vel,Output_theta,curvatura,vel_crucero,curvatura_predecesor,control'+'\n')
+
+texto.close()
+
+while True:
+    data, addr = sock.recvfrom(4096) # buffer size is 1024 byte
+    testo = str(data.decode('utf-8'))
+    lista = testo.split(",")
+    texto = open(file_name,"a")
+    texto.write(testo+'\n')
+    texto.close()
+    print(testo)
+    
